5700740135c79a4480bbb5ed52e03a24ed0f620a
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-08-13 19:02:31 +0100

    Adding our Note list Activity to display our list of notes

---
+++ app/src/main/java/com/example/notekeeperkot/NoteListActivity.kt
@@ -0,0 +1,34 @@
+package com.example.notekeeperkot
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.notekeeperkot.databinding.ActivityNoteListBinding
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+
+        binding.fab.setOnClickListener { view ->
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
