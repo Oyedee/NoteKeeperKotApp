7ae0fffd970fec8679d8b1b0a7644f08101a4f7d
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-08-19 15:30:09 +0100

    Make NoteInfo Class Nullable

--- app/src/main/java/com/example/notekeeperkot/NoteKeeperData.kt
+++ app/src/main/java/com/example/notekeeperkot/NoteKeeperData.kt
@@ -6,4 +6,10 @@ data class CourseInfo (val courseId: String, val title: String) {
     }
 }
 
-data class NoteInfo (var course: CourseInfo, var title: String, var text: String)
+
+/**
+ * Let's make our Note Info Class Nullable
+ * So what that would allow us to do is create a new instance of NoteInfo and pass null for each of
+ * these properties
+ * Also, in addition let's give the properties a default value of null**/
+data class NoteInfo (var course: CourseInfo? = null, var title: String? = null, var text: String? = null)
